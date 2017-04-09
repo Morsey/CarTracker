597ede939553bd26f468bc877df72e37110e9f87
rob <rob@morseweb.co.uk>
2017-04-09 15:22:33 +0100

    tracker first implemented. Hangs on large areas

--- carTracker.py
+++ carTracker.py
@@ -25,26 +25,23 @@ cap = cv2.VideoCapture('test.mp4')
 tracker = cv2.Tracker_create("MEDIANFLOW")
 bytes = bytes()
 
-tracking = False
-oldx = 0
-oldTime =msTime()
+
+
 while True:
 
     ret, i = cap.read()
-    newTime = msTime()
-    height,width = i.shape[:2]
+
+    image_height,image_width = i.shape[:2]
+
     i =  cv2.rotate(i,cv2.ROTATE_180)
     gr = cv2.cvtColor(i,cv2.COLOR_BGR2GRAY)
 
-    #remove region of image outside of road area
-    #roadRaw = cv2.bitwise_and(i, roadMask)
-
+    #get changes
     fgmask = fgbg.apply(gr)
     fgmask = cv2.morphologyEx(fgmask,cv2.MORPH_OPEN,kernel)
-   # fgmask = cv2.morphologyEx(fgmask,cv2.MORPH_CLOSE,kernel)
 
 
-    ret, thresh= cv2.threshold(fgmask,127,255,0)
+    ret, thresh = cv2.threshold(fgmask,127,255,0)
 
     #get countours
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,43 +53,44 @@ while True:
         #We have a box
         (x, y, w, h) = cv2.boundingRect(sortedContours[0])
         bbox = (x,y,w,h)
+        print(" Have contour =", x,":",y,":",w,":",h)
         #only look at big boxes
-        if h > 5 and w > 5:
 
+        if h > 5 and w > 5:
+            print("Have big enough contour")
             #start tracking if whole box in window
-            if x > 5 and x+w < width -5:
+            if x > 5 and x+w < image_width -5:
+                print("Have big enough contour within window")
 
                 ok = tracker.init(i, bbox)
+
                 cv2.rectangle(i, (x, y), (x + w, y + h), (0, 255, 0), 1)
-                print('in frame')
-                while True:
-                    # Read a new frame
-                    ret, frame = cap.read()
-                    frame =  cv2.rotate(frame,cv2.ROTATE_180)
-                    # Update tracker
-                    print("checking")
-                    ok, bbox = tracker.update(frame)
-                    print("checked")
 
-                    # Draw bounding box
-                    if ok:
-                        p1 = (int(bbox[0]), int(bbox[1]))
-                        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                        cv2.rectangle(frame, p1, p2, (0, 0, 255))
-                        cv2.imshow("Tracking", frame)
-                        print("ok")
-                    if not ok:
-                        print("not ok")
-                        break
-                    # Display result
+                if ok:
+                    print("Have initial tracker")
+                    while True:
+                        # Read a new frame
+                        ret, frame = cap.read()
+                        frame =  cv2.rotate(frame,cv2.ROTATE_180)
+                        # Update tracker
+
+                        ok, bbox = tracker.update(frame)
 
 
+                        # Draw bounding box
+                        if ok:
+                            print("Have another track")
+                            p1 = (int(bbox[0]), int(bbox[1]))
+                            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                            cv2.rectangle(frame, p1, p2, (0, 0, 255))
+                            cv2.imshow('col', frame)
+                            cv2.waitKey(1)
+                            #time.sleep(0.5)
 
-        else:
-            if tracking:
-                #finish tracking
-                tracking = False
-
+                        if not ok:
+                            #reset tracker
+                            tracker = cv2.Tracker_create("MEDIANFLOW")
+                            break
 
     cv2.imshow('colour', i)
     cv2.imshow('thresh',thresh)
